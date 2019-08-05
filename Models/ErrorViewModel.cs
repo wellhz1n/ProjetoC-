1268b7e28b7cbc3da38ba9c04942f1362f5b101b
wellhz1n <wellingtom1234520@gmail.com>
2019-08-05 19:50:32 -0300

    inicial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace projetoC_.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
